27a191787819d0e3dc11e7da7d69a00520ef6973
Kevin Oswaldo Cabrera Navarro <kevintroko@gmail.com>
2019-04-23 19:10:35 -0500

    First change

---
+++ pl-project/src/Multithreading/Buffer.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Multithreading;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import Screen.MainPanel;
+
+
+/**
+ *
+ * @author Kevintroko
+ */
+public class Buffer {
+    private Queue<String> bufferStorage;
+    private int bufferLength;
+    private int consumerOperations, producerOperations;
+    private Lock lock = new ReentrantLock();
+    private Condition notFull = lock.newCondition();
+    private Condition notEmpty = lock.newCondition();
+    private MainPanel mainPanel;
+    
+    public Buffer(int length, MainPanel mainPanel) {
+        this.bufferStorage = new LinkedList<String>();
+        this.bufferLength = length;
+        this.consumerOperations = 0;
+        this.producerOperations = 0;
+        this.mainPanel = mainPanel;
+    }
+    
+    public String consume() throws InterruptedException {
+    	System.out.println(bufferStorage.size());
+    	lock.lock();
+    	try {
+	         while(this.bufferStorage.isEmpty()) {
+	        	 notEmpty.await();
+	         }
+	         	String product = this.bufferStorage.poll();
+		        consumerOperations++;
+		        producerOperations--;
+		        mainPanel.addRemainingCounter(producerOperations);
+		        mainPanel.addCompletedCounter(consumerOperations);
+		        
+		        notFull.signal();
+		        
+		        return product;
+    	} finally {
+		lock.unlock();
+	}    
+    }
+    
+    public void produce(String product) throws InterruptedException {
+    	lock.lock();
+        try {
+            while(this.bufferLength == this.bufferStorage.size()) {
+                notFull.await();
+            }
+            this.bufferStorage.add(product);
+            producerOperations++;
+            mainPanel.addRemainingCounter(producerOperations);
+            mainPanel.addRemainingDividedByBufferSize(producerOperations, bufferLength);
+            notEmpty.signal();
+        } finally {
+            lock.unlock();
+        }  
+    } 
+}
